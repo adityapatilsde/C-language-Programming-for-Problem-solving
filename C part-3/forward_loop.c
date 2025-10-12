ea4e6b3965d94b1c383754cd9957899b41efd1a5
aditya <adityapatilsde@gmail.com>
2025-10-12 22:41:59 +0530

    looping in c

---
+++ C part-3/forward_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int count = 1;
+
+    while (count<=5) {  // Fill in the correct condition
+        printf("Count: %d\n", count);
+        count++;        // Fill in the missing increment
+    }
+
+    return 0;
+}
